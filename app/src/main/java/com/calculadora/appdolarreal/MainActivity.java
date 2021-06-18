c6197459b9467f0ec38c12fa26251b822d5c159f
Italo <paradoxonitalo@gmail.com>
2021-06-18 15:56:12 -0300

    - APP-DollarToRealConverter This application was made in Android Studio using the Java language
    Este aplicativo foi feito no Android Studio utilizando a linguagem Java

---
+++ app/src/main/java/com/calculadora/appdolarreal/MainActivity.java
@@ -0,0 +1,44 @@
+package com.calculadora.appdolarreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtValorDolar, edtQuantDolar;
+    TextView txtResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtValorDolar = findViewById(R.id.edtValorDolar);
+        edtQuantDolar = findViewById(R.id.edtQuantDolar);
+        txtResultado = findViewById(R.id.txtResultado);
+
+
+    }
+
+    public void calcular(View view) {
+
+        double quantDolar = Double.parseDouble(edtQuantDolar.getText().toString());
+        double valorDolar = Double.parseDouble(edtValorDolar.getText().toString());
+
+        double totalReais = valorDolar * quantDolar;
+
+        DecimalFormat decimal = new DecimalFormat("0.00");
+        String valorFormatado = decimal.format(totalReais);
+
+        txtResultado.setText("R$: "+valorFormatado);
+
+    }
+
+
+}
